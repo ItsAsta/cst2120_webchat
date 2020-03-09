de53a8c9bf992f86d55d8a3dc8c1f7fa4350ec87
itsAsta <maxedasta@gmail.com>
2020-03-09 00:05:51 +0000

    database for login/register

--- assets/javascripts/client.js
+++ assets/javascripts/client.js
@@ -4,7 +4,10 @@ const messageForm = document.getElementById('sendContainer');
 const messageContainer = document.getElementById('messageContainer');
 const messageInput = document.getElementById('messageInput');
 
-socket.on('chat-message', data => {
+const registerBtn = document.getElementById('registerBtn');
+const loginBtn = document.getElementById('loginBtn');
+
+socket.on('chatMessage', data => {
     appendMessage(`User: ${data}`);
 });
 
@@ -16,19 +19,59 @@ socket.on('allMessages', data => {
     });
 });
 
+registerBtn.addEventListener('click', e => {
+    e.preventDefault();
+
+    let username = $("#registerUsername").val();
+    let confirmUsername = $("#confirmUsername").val();
+    let password = $("#registerPassword").val();
+    let confirmPassword = $("#confirmPassword").val();
+
+    let data = {
+        "username": "",
+        "password": ""
+    };
+
+
+    if (username === confirmUsername && password === confirmPassword) {
+        data.username = username;
+        data.password = password;
+        socket.emit('registerUser', JSON.stringify(data));
+    }
+
+});
+
+loginBtn.addEventListener('click', e => {
+    e.preventDefault();
+
+    let username = $("#loginUsername").val();
+    let password = $("#loginPassword").val();
+
+    let data = {
+        "username": "",
+        "password": ""
+    };
+
+    data.username = username;
+    data.password = password;
+    socket.emit('loginUser', JSON.stringify(data));
+
+});
+
+
 messageForm.addEventListener('submit', e => {
     e.preventDefault();
     const message = messageInput.value;
     appendMessage(`You: ${message}`);
-    socket.emit('send-chat-message', message);
+    socket.emit('sendChatMessage', message);
     messageInput.value = '';
 });
 
-$("body").on('DOMSubtreeModified', messageContainer, function() {
+$("body").on('DOMSubtreeModified', messageContainer, function () {
     updateScroll();
 });
 
-function updateScroll(){
+function updateScroll() {
     let element = document.getElementById('messageContainer');
     element.scrollTop = element.scrollHeight;
 }
--- server.js
+++ server.js
@@ -1,34 +1,65 @@
+// Get required modules for our application
 var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 const mysql = require('mysql');
 
+// Store users in an array
 const users = [];
+
+// Store amount of users connected in an array
 const connections = [];
 
+// Make server listen to a specific port for our connection
 server.listen(process.env.PORT || 3000);
 console.log("Server running on port 3000!");
+
+// Display our html file using the server
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
 
+// Connect to a socket for socket operations
 io.on('connection', socket => {
+   // Add new connection to our socket array
    connections.push(socket);
    console.log('Connected: %s sockets connected!', connections.length);
 
-    // Disconnect
-    socket.on('disconnect', function (data) {
+    // Make socket listen to socket disconnections
+    socket.on('disconnect', data => {
+        // Remove the socket from our connections array
         connections.splice(connections.indexOf(socket), 1);
         console.log('Disconnected: %s sockets connected', connections.length)
     });
 
-    socket.on('send-chat-message', message => {
-        socket.broadcast.emit('chat-message', message);
+    // Make socket listen to a new chat message from our client
+    socket.on('sendChatMessage', message => {
+        // send the message back to the client to display it
+        socket.broadcast.emit('chatMessage', message);
     });
 
-    getEmployees(socket)
+    // Make a socket listen to the client when they click the register button on the form
+    socket.on('registerUser', data => {
+        // Parse the json object to access the elements
+        let parsedData = JSON.parse(data);
+
+        // Execute the database function to add the user to the database
+        registerUser(socket, parsedData.username, parsedData.password);
+    });
+
+    // Make a socket listen to the client when they click the login button on the form
+    socket.on('loginUser', data => {
+        // Parse the json object to access the elements
+        let parsedData = JSON.parse(data);
+
+        // Execute the database function to add the user to the database
+        loginUser(socket, parsedData.username, parsedData.password);
+    });
+
+    // Execute the database function responsible to fetch all the chat messages
+    getChat(socket)
 });
 
 //Create a connection pool with the user details
@@ -43,9 +74,9 @@ const connectionPool = mysql.createPool({
 });
 
 
-/* Outputs all of the employees */
-function getEmployees(socket){
-    //Build query
+/* Outputs all of the chat */
+function getChat(socket){
+    //Build query to select the chats from the database
     let sql = "SELECT * FROM chats";
 
     //Execute query and output results
@@ -56,6 +87,58 @@ function getEmployees(socket){
         else{//Output results in JSON format - a web service would return this string.
 
             socket.emit('allMessages', result)
+        }
+    });
+}
+
+function registerUser(socket, username, password) {
+    let query = "INSERT INTO users (username, password) " +
+        " VALUES ('" + username + "', '" + password + "')";
+
+    let checkUserQuery = "SELECT * FROM users WHERE username = '" + username + "'";
+
+    connectionPool.query(checkUserQuery, (err, result) => {
+        if (err){//Check for errors
+            console.error("Error executing query: " + JSON.stringify(err));
+        }
+
+        // Checks the length of the result to see if our query has found a user with the same username registered
+        console.log(result);
+        if (result.length > 0) {
+            //Handle that the username has already been registered.
+            console.log("User already registered!")
+        } else {
+            //Execute query and register user
+            connectionPool.query(query, (err, result) => {
+                if (err){//Check for errors
+                    console.error("Error executing query: " + JSON.stringify(err));
+                } else {
+                    console.log("Registered User")
+                }
+            });
+        }
+    });
+}
+
+function loginUser(socket, username, password) {
+
+    // Make a query to check if we got a username and password that match what the user has entered
+    let checkUserQuery = "SELECT * FROM users WHERE username = '" + username + "' AND password = '" + password + "'";
+
+    connectionPool.query(checkUserQuery, (err, result) => {
+        //Check for errors
+        if (err){
+            console.error("Error executing query: " + JSON.stringify(err));
+        }
+
+        // Checks the length of the result to see if our query has found a user with the same username registered
+        console.log(result);
+        if (result.length > 0) {
+            //Handle login
+            console.log("User logged in!")
+        } else {
+            //Handle user not registered
+            console.log("User not found!")
         }
     });
 }
